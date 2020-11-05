081bd619a26d032de7a3ba85c2c12e7c6f927268
Nathan Hughes <na26933@mit.edu>
2020-11-05 04:01:25 +0000

    changes to prep for data collection tomorrow

--- src/intel_aero_ros/interp_trajectory_tracker.py
+++ src/intel_aero_ros/interp_trajectory_tracker.py
@@ -7,14 +7,21 @@ class InterpTrajectoryTracker:
     """Class for tracking a trajectory."""
 
     def __init__(
-        self, polynomial, lengths, gripper_latency=0.0, start_gripper_early=False
+        self,
+        polynomial,
+        lengths,
+        gripper_latency=0.0,
+        settle_time=None,
     ):
         """Set everything up."""
         self._polynomial = polynomial
         self._lengths = lengths
         self._total_time = self._polynomial._total_time
         self._gripper_latency = gripper_latency
-        self._start_gripper_early = start_gripper_early
+        if settle_time is not None:
+            self._final_time = self._total_time + settle_time
+        else:
+            self._final_time = self._total_time
 
         self._start_time = None
 
@@ -27,12 +34,12 @@ class InterpTrajectoryTracker:
         return self._polynomial.interp(self._total_time)[0]
 
     def _run_normal(self, t):
-        """Don't bother trying to do the full gripper trajectory."""
+        """Interpolate the trajectory."""
         if t > self._total_time:
             return MissionRunResult(
                 self._polynomial.interp(self._total_time)[0],
                 lengths=self._lengths.interp(self._total_time),
-                finished=True,
+                finished=(t > self._final_time),
             )
 
         pos, vel, acc = self._polynomial.interp(t)
@@ -45,36 +52,10 @@ class InterpTrajectoryTracker:
             finished=False,
         )
 
-    def _run_full_gripper(self, t):
-        """Run the full gripper trajectory."""
-        if t > self._total_time + self._gripper_latency:
-            return MissionRunResult(
-                self._polynomial.interp(self._total_time)[0],
-                lengths=self._lengths.interp(self._total_time),
-                finished=True,
-            )
-
-        if t <= self._gripper_latency:
-            pos, vel, acc = self._polynomial.interp(0)
-        else:
-            pos, vel, acc = self._polynomial.interp(t - self._gripper_latency)
-
-        return MissionRunResult(
-            pos,
-            yaw=0.0,
-            velocity=vel,
-            acceleration=acc,
-            lengths=self._lengths.interp(t),
-            finished=False,
-        )
-
     def run(self, current_position):
         """State handler for EXECUTING_MISSION."""
         if self._start_time is None:
             self._start_time = rospy.Time.now()
 
         t = (rospy.Time.now() - self._start_time).to_sec()
-        if self._start_gripper_early:
-            return self._run_full_gripper(t)
-        else:
-            return self._run_normal(t)
+        return self._run_normal(t)
