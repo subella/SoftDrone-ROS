a77c8cdd70139770a12aad1acb9fdecd8e5becf4
Samuel Ubellacker <subella@mit.edu>
2022-09-06 13:16:41 -0400

    Update keypoint detector for cropped images.

--- softdrone_target_pose_estimator/src/keypoint_client.py
+++ softdrone_target_pose_estimator/src/keypoint_client.py
@@ -24,6 +24,9 @@ import torchvision
 from trt_pose.coco import *
 import time
 
+NUM_KEYPOINTS = 7
+IMAGE_AREA = 234000
+
 def send_array(socket, A, flags=0, copy=True, track=False):
     """send a numpy array with metadata"""
     md = dict(
@@ -35,12 +38,19 @@ def send_array(socket, A, flags=0, copy=True, track=False):
 
 def detect_keypoints(cv2_image, model):
     cv2_image = cv2_image[:,:,::-1]
-    image_shape = (640, 360)
-    image = Image.fromarray(cv2_image).convert("RGB")
+    #image_shape = (640, 360)
+    orig_image = Image.fromarray(cv2_image).convert("RGB")
+    orig_image = orig_image.crop((0, 
+                                  0.5 * orig_image.height,
+                                  orig_image.width, 
+                                  orig_image.height))
+            
+    resize_factor = np.sqrt((orig_image.width * orig_image.height) / IMAGE_AREA)
+    image_shape = (int(orig_image.width / resize_factor), int(orig_image.height / resize_factor))
     #ar = float(image.width) / float(image.height)
     ar = 1.0
     quad = get_quad(0.0, (0, 0), 1.0, aspect_ratio=ar)
-    image = transform_image(image, image_shape, quad)
+    image = transform_image(orig_image, image_shape, quad)
     #image.save("img.png")
     data = transform(image).cuda()[None, ...]
     cmap = model(data)
@@ -48,11 +58,11 @@ def detect_keypoints(cv2_image, model):
     object_counts, objects, peaks = parse_objects(cmap, cmap)
     object_counts, objects, peaks = int(object_counts[0]), objects[0], peaks[0]
 
-    kps = [0]*(33*3)
+    kps = [0]*(NUM_KEYPOINTS*3)
     for i in range(object_counts):
         object = objects[i]
         cnt = 0
-        for j in range(33):
+        for j in range(NUM_KEYPOINTS):
             k = object[j]
             if k >= 0:
                 peak = peaks[j][k]
@@ -63,15 +73,15 @@ def detect_keypoints(cv2_image, model):
                     x = (peak[1] - 0.5) / ar + 0.5
                     y = peak[0]
 
-                x = round(float(1280 * x))
-                y = round(float(720 * y))
+                x = round(float(orig_image.width * x))
+                y = round(float(orig_image.height * y + orig_image.height))
                 kps[j * 3 + 0] = x
                 kps[j * 3 + 1] = y
                 kps[j * 3 + 2] = 2
-    return np.array(kps).reshape((33,3))
+    return np.array(kps).reshape((NUM_KEYPOINTS,3))
 
 if __name__=="__main__":
-    OPTIMIZED_MODEL = 'softdrone5.pth'
+    OPTIMIZED_MODEL = 'softdrone_cropped.pth'
     model = TRTModule()
     model.load_state_dict(torch.load(OPTIMIZED_MODEL))
     print("Model loaded!")
@@ -82,7 +92,7 @@ if __name__=="__main__":
     topology = trt_pose.coco.coco_category_to_topology(target_pose)
     transform = torchvision.transforms.Compose([
                 torchvision.transforms.ToTensor(),
-                torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                torchvision.transforms.Normalize([0.42174017, 0.39773076, 0.40150955], [0.25029679, 0.23943031, 0.25025475])
         ])
     parse_objects = ParseObjects(topology, cmap_threshold=0.1, link_threshold=0.1, cmap_window=5, line_integral_samples=7, max_num_parts=100, max_num_objects=100)
     
