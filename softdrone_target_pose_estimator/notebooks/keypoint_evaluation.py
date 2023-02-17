288f810687a9b2c31fd9c28c3ffdf6708fc4ebf3
subella <subella@mit.edu>
2023-02-17 17:55:39 -0500

    Improved plotting abilities.

--- softdrone_target_pose_estimator/notebooks/keypoint_evaluation.py
+++ softdrone_target_pose_estimator/notebooks/keypoint_evaluation.py
@@ -25,8 +25,13 @@ import matplotlib.pyplot as plt
 import cv2
 import teaserpp_python
 import seaborn as sns
+import pandas as pd
+import random
+import pickle
+
 from tqdm import tqdm
 from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.axes_grid1 import ImageGrid
 from mpl_toolkits.mplot3d import proj3d
 from mpl_toolkits.mplot3d import Axes3D
 from PIL import Image, ImageDraw, ImageOps
@@ -64,7 +69,6 @@ class SingleEvaluator():
         self.cad_keypoints = cad_keypoints
         self.verbose = verbose
 
-        
         self.solver = self.get_teaser()
         
         self.gt_pixel_keypoints = self.get_gt_pixel_keypoints()
@@ -79,7 +83,7 @@ class SingleEvaluator():
         self.world_keypoints_error = self.compute_world_keypoints_error()
         self.rotation_error, self.translation_error = self.compute_pose_error()
         
-        self.return_stats(verbose=True)
+#         self.return_stats(verbose=True)
 
     def get_teaser(self):
         solver_params = teaserpp_python.RobustRegistrationSolver.Params()
@@ -136,21 +140,20 @@ class SingleEvaluator():
         return self.world_keypoints_error / len(self.gt_pixel_keypoints)
     
     def compute_pose_error(self):
-        gt_rotation = R.from_matrix(self.gt_teaser_pose[:3,:3])
-        est_rotation = R.from_matrix(self.est_teaser_pose[:3,:3])
-        gt_euler = gt_rotation.as_euler('zyx', degrees=True)
-        est_euler = est_rotation.as_euler('zyx', degrees=True)
-        error_euler = gt_euler - est_euler
-        error_translation = self.gt_teaser_pose[:3,3] - self.est_teaser_pose[:3,3]
-        return error_euler, error_translation
+        gt_rotation = self.gt_teaser_pose[:3,:3]
+        est_rotation = self.est_teaser_pose[:3,:3]
+        rotation_error = np.arccos((np.trace(est_rotation.T.dot(gt_rotation)) - 1) / 2)
+        rotation_error = np.rad2deg(rotation_error)
+        translation_error = np.linalg.norm(self.gt_teaser_pose[:3,3] - self.est_teaser_pose[:3,3])
+        return rotation_error, translation_error
         
-    def return_stats(self, verbose=False):
+    def get_df(self, verbose=False):
         stats = {}
-        stats["average_keypoint_pixel_error"] = self.pixel_keypoints_error
-        stats["average_keypoint_world_error"] = self.world_keypoints_error
-        stats["translation_error"] = self.translation_error
-        stats["rotation_error"] = self.rotation_error
-        stats["distance"] = self.distance
+        stats["average_keypoint_pixel_error"] = [self.pixel_keypoints_error]
+        stats["average_keypoint_world_error"] = [self.world_keypoints_error]
+        stats["translation_error"] = [self.translation_error]
+        stats["rotation_error"] = [self.rotation_error]
+        stats["distance"] = [self.distance]
         if self.verbose:
             print("Average Keypoint Pixel Error: ", self.pixel_keypoints_error)
             print("Average Keypoint World Error: ", self.world_keypoints_error)
@@ -158,16 +161,18 @@ class SingleEvaluator():
             print("Translation Error", self.translation_error)
             print("gt", self.gt_teaser_pose)
             print("est", self.est_teaser_pose)
+        return pd.DataFrame(stats)
         
 
 # -
 
-class SinglePlotter(SingleEvaluator):
-    def __init__(self, drawing, *args, **kwargs):
-        self.drawing = drawing
+class SinglePlotter():
+    def __init__(self, evaluator):
+        self.drawing = ImageDraw.Draw(evaluator.rgb_image)
+        self.image = evaluator.rgb_image.copy()
+        self.eval = evaluator
         self.fig_3d = None
         self.ax_3d = None
-        super().__init__(*args, **kwargs)
     
     def init_fig_3d(self):
         # %matplotlib inline
@@ -178,15 +183,27 @@ class SinglePlotter(SingleEvaluator):
         self.ax_3d.legend()
         plt.show()
         
-    def crop_object(self):
-        min_x, min_y = np.min(self.gt_pixel_keypoints, axis=0)
-        max_x, max_y = np.max(self.gt_pixel_keypoints, axis=0)
-        x_padding = 2 * (max_x - min_x)
-        y_padding = 2 * (max_y - min_y)
-        cropped_image = self.rgb_image.crop((min_x - x_padding, min_y - y_padding, 
-                                         max_x + x_padding, max_y + y_padding))
-        image = ImageOps.contain(cropped_image, (self.width, self.height))
-        return image
+    def show_image(self):
+        display(self.image)
+        
+    def crop_image(self):
+        center = np.mean(self.eval.gt_pixel_keypoints, axis=0)
+        half_width = 150
+        half_height = 150
+        
+        if center[0] - half_width < 0:
+            center[0] = half_width
+        elif center[0] + half_width > self.image.width:
+            center[0] = self.image.width - half_width
+            
+        if center[1] - half_height < 0:
+            center[1] = half_height
+        elif center[1] + half_height > self.image.height:
+            center[1] = self.image.height - half_height
+            
+        cropped_image = self.eval.rgb_image.crop((center[0] - half_width, center[1] - half_height, 
+                                                  center[0] + half_width, center[1] + half_height))
+        self.image = ImageOps.contain(cropped_image, (self.eval.width, self.eval.height))
         
     def plot_keypoints(self, keypoints, r=2, color=(0,255,0)):
         for keypoint in keypoints:
@@ -194,95 +211,154 @@ class SinglePlotter(SingleEvaluator):
             self.drawing.ellipse((x-r, y-r, x+r, y+r), fill=color)
         
     def plot_gt_pixel_keypoints(self, r=2, color=(0,255,0)):
-        self.plot_keypoints(self.gt_pixel_keypoints, r, color)
+        self.plot_keypoints(self.eval.gt_pixel_keypoints, r, color)
 
     def plot_est_pixel_keypoints(self, r=2, color=(255,0,0)):
-        self.plot_keypoints(self.est_pixel_keypoints, r, color)        
+        self.plot_keypoints(self.eval.est_pixel_keypoints, r, color)        
         
     def plot_gt_world_keypoints(self):
         if not self.fig_3d:
             self.init_fig_3d()
-        points = self.gt_world_keypoints.T
+        points = self.eval.gt_world_keypoints.T
         self.ax_3d.scatter(points[0], points[1], points[2],
                            c='green', label='Ground Truth Point Cloud')        
     
     def plot_est_world_keypoints(self):
         if not self.fig_3d:
             self.init_fig_3d()
-        points = self.est_world_keypoints.T
+        points = self.eval.est_world_keypoints.T
         self.ax_3d.scatter(points[0], points[1], points[2],
                            c='red', label='Estimated Point Cloud')
     
     def plot_gt_teaser_pose(self, scale=100):
-        plot_pose(self.drawing, self.gt_teaser_pose, self.K)
+        plot_pose(self.drawing, self.eval.gt_teaser_pose, self.eval.K)
     
     def plot_est_teaser_pose(self):
-        plot_pose(self.drawing, self.est_teaser_pose, self.K, 
+        plot_pose(self.drawing, self.eval.est_teaser_pose, self.eval.K, 
                   x_color=(139,0,0), y_color=(0,139,0), z_color=(0,0,139))
 
 
 
 # +
 class MultiEvaluator():
-    def __init__(self, verbose=False):
-        self.data = []
-#         self.run(folder, verbose)
+    def __init__(self, folder, verbose=False):
+        self.folder = folder
+        self.evaluators = []
+        self.df = pd.DataFrame({"average_keypoint_pixel_error":[],
+                                "average_keypoint_world_error":[],
+                                "translation_error":[],
+                                "rotation_error":[],
+                                "distance":[]})
+        self.verbose = verbose
         
-    def run(self, folder, verbose=False):
-        for folder in glob.glob(working_folder + "/*"):                                       
-            metadata_path = folder + "/metadata.json"
-            f = open(metadata_path)
-            metadata = json.load(f)
-            K = np.array(metadata["intrinsics"]).T
-            cad_keypoints = np.array(metadata["cad_frame"])
+    def parse_metadata(self, folder):
+        metadata_path = folder + "/metadata.json"
+        f = open(metadata_path)
+        metadata = json.load(f)
+        # Distinction here:
+        # K_mat is calibrated with matlab specifically on this dataset
+        # (it is more accurate for ground truth / overfit)
+        # K_ros is what ros gives for the camera 
+        # (what we actually use on the robot)
+        K_mat = np.array(metadata["intrinsics"]["intrinsic_matrix"])
+        K_ros = np.array(metadata["ros_intrinsics"]["intrinsic_matrix"])
+        cad_keypoints = np.array(metadata["cad_frame"])
+        return metadata, K_mat, K_ros, cad_keypoints
+        
+    def eval_annotation(self, folder, annotation, K_mat, K_ros, cad_keypoints):
+        rgb_image_path = folder + "/" + annotation["rgb_file_name"]
+        rgb_image = Image.open(rgb_image_path).convert('RGB')
+        depth_image_path = folder + "/" + annotation["depth_file_name"]
+        depth_image = cv2.imread(depth_image_path, cv2.IMREAD_UNCHANGED)
+        evaluator = SingleEvaluator(model, cad_keypoints, rgb_image, depth_image, 
+                                    annotation, K_mat, verbose=self.verbose)
+        self.evaluators.append(evaluator)
+        self.df = self.df.append(evaluator.get_df())
+
+        
+    def run(self):
+        for folder in glob.glob(self.folder + "/*"):                                       
+            metadata, K_mat, K_ros, cad_keypoints = self.parse_metadata(folder)
             print("Starting folder :", folder)
             for annotation in tqdm.tqdm(metadata["annotations"]):
-                rgb_image_path = folder + "/" + annotation["rgb_file_name"]
-                rgb_image = Image.open(rgb_image_path).convert('RGB')
-                depth_image_path = folder + "/" + annotation["depth_file_name"]
-                depth_image = cv2.imread(depth_image_path, cv2.IMREAD_UNCHANGED)
-
-                drawing = ImageDraw.Draw(rgb_image)
-                plotter = SinglePlotter(drawing, model, cad_keypoints, rgb_image, depth_image, 
-                                        annotation, K, verbose=verbose)
-                self.data.append(plotter.return_stats())
-                if verbose:
-                 plotter.plot_gt_pixel_keypoints()
-                 plotter.plot_est_pixel_keypoints()
-                 plotter.plot_gt_teaser_pose()
-                 plotter.plot_est_teaser_pose()
-                 plotter.plot_gt_world_keypoints()
-                 plotter.plot_est_world_keypoints()
-                 plotter.finalize_fig_3d()
-                 image = plotter.crop_object()
-                 display(image)
+                self.eval_annotation(folder, annotation, K_mat, K_ros, cad_keypoints)
     
     def evaluate(self):
         pass
         
     
 
-
-# +
-class MultiPlotter(MultiEvaluator):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    
-    def plot_keypoint_error_vs_distance(self):
-        fig = plt.figure()
-        for data_point in self.data:
-            
-#         for data_point in self.
-
-
 # -
+
+class MultiPlotter():
+    def __init__(self, multi_evaluator):
+        self.multi_evaluator = multi_evaluator
+        
+    def eval_annotation(self, folder, annotation):
+        evaluator = super().eval_annotation(folder, annotation)
+        Single
+        if self.verbose:
+            self.cur_evaluator.plot_gt_pixel_keypoints()
+            self.cur_evaluator.plot_est_pixel_keypoints()
+            self.cur_evaluator.plot_gt_teaser_pose()
+            self.cur_evaluator.plot_est_teaser_pose()
+            self.cur_evaluator.plot_gt_world_keypoints()
+            self.cur_evaluator.plot_est_world_keypoints()
+            self.cur_evaluator.finalize_fig_3d()
+            image = self.cur_evaluator.crop_object()
+            display(image)
+        
+    
+    def plot_keypoint_pixel_error_vs_distance(self):
+        fig = plt.figure()
+        self.multi_evaluator.df.plot(x="distance", y="average_keypoint_pixel_error")
+        
+    def plot_translation_error_vs_distance(self):
+        fig = plt.figure()
+        self.multi_evaluator.df.plot(x="distance", y="translation_error")
+    
+    def plot_rotation_error_vs_distance(self):
+        fig = plt.figure()
+        self.multi_evaluator.df.plot(x="distance", y="rotation_error")
+          
+    def plot_multiple(self, rows=3, cols=3):
+        # %matplotlib inline
+
+        fig = plt.figure(1,(20,20))
+        grid = ImageGrid(fig, 111,
+                         nrows_ncols=(rows,cols),
+                         axes_pad=0.1,
+                         share_all=True
+                         )
+        grid[0].get_yaxis().set_ticks([])
+        grid[0].get_xaxis().set_ticks([])
+
+        for i in range(rows * cols):
+            evaluator = random.choice(self.multi_evaluator.evaluators)
+            plotter = SinglePlotter(evaluator)
+            plotter.plot_gt_pixel_keypoints()
+            plotter.plot_est_pixel_keypoints()
+            plotter.plot_gt_teaser_pose()
+            plotter.plot_est_teaser_pose()
+            plotter.crop_image()
+            grid[i].imshow(plotter.image)
 
 data_folder_array = []
 for data_folder in glob.glob(working_folder + "/*"):                                       
     data_folder_array.append(data_folder)
 
-multi_plotter = MultiPlotter()
-multi_plotter.run(working_folder)
+multi_evaluator = MultiEvaluator(working_folder, verbose=False)
+multi_evaluator.run()
+
+multi_evaluator.df
+
+multi_plotter = MultiPlotter(multi_evaluator)
+multi_plotter.plot_multiple()
+multi_plotter.plot_keypoint_pixel_error_vs_distance()
+multi_plotter.plot_translation_error_vs_distance()
+multi_plotter.plot_rotation_error_vs_distance()
+
+multi_plotter.df
 
 for folder in data_folder_array:
     metadata_path = folder + "/metadata.json"
